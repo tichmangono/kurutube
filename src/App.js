eb98d9c47d432294df44c15722ec28212c9f8eeb
tichmangono <tichmangono@gmail.com>
2023-02-25 14:05:22 -0500

    video app first commit with folder setup and navbar

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {Box} from '@mui/material'
+
+import {Navbar, Feed, VideoDetail, SearchFeed, ChannelDetail} from "./components"
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+     <Box sx={{backgroundColor: '#000'}}>
+       <Navbar/>
+       <Routes>
+        <Route path="/" exact element = {<Feed/>}></Route>
+        <Route path="/video/:id" element = {<VideoDetail/>}></Route>
+        <Route path="/channel/:id" element = {<ChannelDetail/>}></Route>
+        <Route path="/search/:searchTerm" element = {<SearchFeed/>}></Route>
+       </Routes>
+     </Box>
+    </BrowserRouter>
+
+  )
+}
+
+export default App
